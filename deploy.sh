c1dcd4a6f5a2d276235efceec3be6afe47af4b2b
Любов Кирилл <thrasher9@yandex.ru>
2026-03-30 22:50:41 +0500

    123

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+docker stop nginx-cont
+docker rm nginx-cont
+docker build -t devops/nginx-server ./nginx
+docker run -d --name nginx-cont -p 54321:80 --restart unless-stopped devops/nginx-server
+docker ps -a
+sleep 5 
+curl 127.0.0.1:54321
+docker logs -n 10 nginx-cont
